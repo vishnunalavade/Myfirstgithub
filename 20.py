6930a62062e819aaa25239fecd565c6a9a24e7be
vishnunalavade <138228857+vishnunalavade@users.noreply.github.com>
2023-07-20 15:41:02 +0530

    Add files via upload

---
+++ 20.py
@@ -0,0 +1,27 @@
+n = 5
+for i in range( 1 ,n+1):
+    for j in range(1, i+1):
+        print("*" ,end=" ")
+    print()
+
+
+n = 5
+for i in range(1,n+1):
+    for j in range(1,i+1):
+         print("*",end=" ")
+    print()
+
+
+n =5
+for i in range(5,0,-1):
+    for j in range(i):
+        print('*', end='')
+    print()
+
+
+n=5
+for i in range(1, n+1):  
+    for j in range(1, i + 1):  
+        print(j, end=' ')  
+    print("")     
+
